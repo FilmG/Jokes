216f4558e6d600c871b111cecead938b3bb537db
FilmG <143239779+FilmG@users.noreply.github.com>
2024-04-12 10:04:21 +0800

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [jokes, setJokes] = useState<any[]>([]);
+  const [limit, setLimit] = useState<number>();
+
+  const getJokes = async () => {
+    try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/dadjokes', {
+            params: {
+                limit: limit
+            },
+            headers: {
+                'X-Api-Key': 'HG4tX1CpNOujUDfwODRlCg==16v6kLGim2O29RU6'
+            }
+        });
+        setJokes(response.data);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Enter limit</h3>
+        <input type="number" min="1" max="10" value={limit} onChange={(e) => setLimit(Number(e.target.value))} />
+        <button onClick={getJokes}>Get Jokes</button>
+        <ul>
+          {jokes.map((joke, index) => (
+            <li key={index}>{joke.joke}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
