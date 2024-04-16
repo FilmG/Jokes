647fe59df4a5951ee0becc7c36c1761efdd883f4
FilmG <143239779+FilmG@users.noreply.github.com>
2024-04-16 23:07:51 +0800

    Added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -29,17 +29,21 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Enter limit</h3>
-        <input type="number" min="1" max="10" value={limit} onChange={(e) => setLimit(Number(e.target.value))} />
-        <button onClick={getJokes}>Get Jokes</button>
-        <ul>
+    <div className="mt-5 flex align-center justify-center">
+      <div className="flex flex-col shadow max-w-96">
+        <label htmlFor="jokes" className="text-center">Enter number of jokes</label>
+        <div className="flex flex-row mx-5 justify-center mb-2">
+        <input className="w-32 shadow appearance-none border rounded-md border-gray-300 m-1 p-1" name="jokes" type="number" min="1" max="10" value={limit} onChange={(e) => setLimit(Number(e.target.value))}/>
+        <button className="border bg-blue-500 text-white rounded p-1 text-xs" onClick={getJokes}>Get Jokes</button>
+        </div>
+        <ul className="w-full">
           {jokes.map((joke, index) => (
-            <li key={index}>{joke.joke}</li>
+            <div className="border">
+              <li className="mx-2" key={index}>{joke.joke}</li>
+            </div>
           ))}
         </ul>
-      </header>
+        </div>
     </div>
   );
 }
